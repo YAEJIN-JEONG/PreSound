ea432e36322707a5507e1118b5eec4624cb0827a
YEJIN-JEONG <wls7367@naver.com>
2021-11-28 01:18:00 +0900

    STT 페이지, CNN 페이지 이동

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def home():  # put application's code here
     return render_template('index.html')
 
 
+@app.route('/stt.html',methods=['GET','POST'])
+def stt():  # put application's code here
+    return render_template('stt.html')
+
+
+@app.route('/ml.html',methods=['GET','POST'])
+def ml():  # put application's code here
+    return render_template('ml.html')
+
+
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
